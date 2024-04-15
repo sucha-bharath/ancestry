1fdb40476ea744fddede7f375f3cb7d354d88242
sucha-bharath <163990890+sucha-bharath@users.noreply.github.com>
2024-04-15 14:55:43 +0530

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inlineSource: '.(js|css)$',
+      template: 'src/template.html',
+    }),
+    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
+  ]
+};
